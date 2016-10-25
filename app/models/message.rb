df50069df51004906a0cb74b875e30d232c786c9
Shinobu Sugimoto <hoshifuruokani@gmail.com>
2016-10-25 12:29:37 +0000

    バリデーション追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    #名前は必須入力かつ2文字以上25文字以内
+    validates :name,length:{minimum:2,maximum:25},presence:true
+    #内容は必須入力かつ2文字以上
+    validates :body,length:{minimum:2},presence:true
 end
