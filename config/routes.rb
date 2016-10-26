4d4d241938ad2752ae8e8fc213312d0e0a342627
Shinobu Sugimoto <hoshifuruokani@gmail.com>
2016-10-26 10:15:08 +0000

    ルート設定変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'messages#index'
+  #root 'messages#index'
+  root :to => 'messages#index'
   resources :messages , except: [:index, :new]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
