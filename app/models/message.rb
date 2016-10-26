246e2307199cf87a27cca7f653a2f39f7c7c7398
Shinobu Sugimoto <hoshifuruokani@gmail.com>
2016-10-26 12:52:06 +0000

    年齢追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
     #名前は必須入力かつ2文字以上25文字以内
     validates :name,length:{minimum:2,maximum:25},presence:true
+    #年齢は0以上
+    validates :age,numericality:{greater_than_or_equal_to:0},presence:true
     #内容は必須入力かつ2文字以上
     validates :body,length:{minimum:2},presence:true
 end
